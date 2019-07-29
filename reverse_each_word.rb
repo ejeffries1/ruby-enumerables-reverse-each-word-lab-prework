c12904577124a2a0d8d4ead2a37ad446fa7d0681
Evan Jeffries <ejeffries14@gmail.com>
2019-07-29 08:31:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,7 @@
 def reverse_each_word(phrase)
   new_array = []
-  phrase.split.each do |words|
+  phrase.split.collect do |words|
     new_array << words.reverse
   end
   new_array.join(" ")
-end
-
-def reverse_collect_word(phrase)
-  phrase.collect do |word|
-    word
-  end
 end
