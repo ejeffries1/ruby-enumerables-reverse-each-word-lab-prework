74292ce1890291dd631d2ae0ce4379c53cf4e886
Evan Jeffries <ejeffries14@gmail.com>
2019-07-29 08:26:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(phrase)
   new_array = []
-  phrase.each do |words|
-    new_array.push(words)
+  phrase.split.each do |words|
+    new_array << words.reverse
   end
+  new_array
 end
